5cd6e723e745f614b25946715e37de6aaa841cb2
Jason Nam <contact@jasonnam.com>
2017-04-28 17:30:46 +0700

    Configure section header

--- iMessage-Stickers/Main/StickerCollectionViewDataSource.swift
+++ iMessage-Stickers/Main/StickerCollectionViewDataSource.swift
@@ -14,13 +14,23 @@ final class StickerCollectionViewDataSource: NSObject, UICollectionViewDataSourc
         return Stickers.count
     }
 
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionElementKindSectionHeader else { return UICollectionReusableView() }
+
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader,
+                                                        withReuseIdentifier: StickerCollectionViewHeader.reuseIdentifier,
+                                                                        for: indexPath) as! StickerCollectionViewHeader
+        header.configure(Stickers[indexPath.section])
+        return header
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return Stickers[section].count
+        return Stickers[section].stickers.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StickerCellReuseIdentifier, for: indexPath) as! StickerCell
-        cell.configure(with: Stickers[indexPath.section][indexPath.row])
+        cell.configure(with: Stickers[indexPath.section].stickers[indexPath.row])
         return cell
     }
 }
