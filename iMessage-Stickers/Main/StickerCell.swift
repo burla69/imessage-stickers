7df45ac1ce3c32810fd5630a668923b46ba18e18
Jason Nam <contact@jasonnam.com>
2017-04-28 16:58:54 +0700

    Add sticker collection view cell

---
+++ iMessage-Stickers/Main/StickerCell.swift
@@ -0,0 +1,39 @@
+//
+//  StickerCell.swift
+//  iMessage-Stickers
+//
+//  Created by Nam Seok Hyeon on 4/28/17.
+//  Copyright © 2017 Nimbl3. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+let StickerCellReuseIdentifier = "StickerCellReuseIdentifier"
+
+final class StickerCell: UICollectionViewCell {
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .green
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.addSubview(nameLabel)
+        nameLabel.snp.remakeConstraints { make in
+            make.top.left.bottom.right.equalToSuperview()
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with sticker: Sticker) {
+        nameLabel.text = sticker.name
+    }
+}
